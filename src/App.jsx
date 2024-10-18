ba0eff1aacc8a6c3a24e6e585c4a070ac8b5d578
NeeraHomeStay <adisharma151ays@gmail.com>
2024-10-19 01:31:41 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,44 @@ import MacContainer from "./MacContainer";
 const App = () => {
   return (
     <div className="w-full h-screen bg-gradient-to-b from-gray-900 to-gray-800">
-      <div className="flex flex-col justify-center items-center h-2/5 space-y-4">
+      <nav className="bg-transparent p-5 ">
+        <div className="container mx-auto flex justify-between items-center">
+          <div className="text-white font-bold text-2xl tracking-wide">
+            MacBook Pro
+          </div>
+          <ul className="flex space-x-8">
+            <li>
+              <a
+                href="#"
+                className="text-white hover:text-gray-300 transition duration-300 ease-in-out text-sm uppercase tracking-wider">
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="text-white hover:text-gray-300 transition duration-300 ease-in-out text-sm uppercase tracking-wider">
+                Features
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="text-white hover:text-gray-300 transition duration-300 ease-in-out text-sm uppercase tracking-wider">
+                Specs
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="bg-white text-gradient-to-b from-gray-900 to-gray-800 px-4 py-2 rounded-full hover:text-gray-900 transition duration-300 ease-in-out text-sm uppercase tracking-wider font-semibold">
+                Buy Now
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className="flex flex-col justify-center items-center h-2/5 space-y-4 -my-10">
         <h3 className="text-center text-white text-7xl font-bold">
           MacBook Pro
         </h3>
